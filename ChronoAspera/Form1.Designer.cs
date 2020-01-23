e43b4029571ede8280a0cbd1f9a9dc632f20291f
william <william.demallmann@gmail.com>
2020-01-23 16:01:54 +0100

    Push initial

---
+++ ChronoAspera/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ChronoAspera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cStart = new System.Windows.Forms.Button();
+            this.sChrono = new System.Windows.Forms.Button();
+            this.labelChrono = new System.Windows.Forms.Label();
+            this.chronoTimer = new System.Windows.Forms.Timer(this.components);
+            this.rChono = new System.Windows.Forms.Button();
+            this.labelAction = new System.Windows.Forms.Label();
+            this.comboBoxActions = new System.Windows.Forms.ComboBox();
+            this.labelAddAction = new System.Windows.Forms.Label();
+            this.textBoxAddAction = new System.Windows.Forms.TextBox();
+            this.buttonAddAction = new System.Windows.Forms.Button();
+            this.SaveAction = new System.Windows.Forms.Button();
+            this.saveFileDialogAction = new System.Windows.Forms.SaveFileDialog();
+            this.saveActionTextbox = new System.Windows.Forms.TextBox();
+            this.updateAction = new System.Windows.Forms.Button();
+            this.labelListAction = new System.Windows.Forms.Label();
+            this.textBoxListAction = new System.Windows.Forms.TextBox();
+            this.importSaveAction = new System.Windows.Forms.Button();
+            this.openFileDialogImportAction = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // cStart
+            // 
+            this.cStart.Location = new System.Drawing.Point(77, 287);
+            this.cStart.Name = "cStart";
+            this.cStart.Size = new System.Drawing.Size(75, 37);
+            this.cStart.TabIndex = 1;
+            this.cStart.Text = "Start";
+            this.cStart.UseVisualStyleBackColor = true;
+            this.cStart.Click += new System.EventHandler(this.cStart_Click);
+            // 
+            // sChrono
+            // 
+            this.sChrono.Location = new System.Drawing.Point(241, 287);
+            this.sChrono.Name = "sChrono";
+            this.sChrono.Size = new System.Drawing.Size(75, 37);
+            this.sChrono.TabIndex = 2;
+            this.sChrono.Text = "Stop";
+            this.sChrono.UseVisualStyleBackColor = true;
+            this.sChrono.Click += new System.EventHandler(this.sChrono_Click);
+            // 
+            // labelChrono
+            // 
+            this.labelChrono.AutoSize = true;
+            this.labelChrono.Location = new System.Drawing.Point(152, 195);
+            this.labelChrono.Name = "labelChrono";
+            this.labelChrono.Size = new System.Drawing.Size(90, 17);
+            this.labelChrono.TabIndex = 3;
+            this.labelChrono.Text = "Chronométre";
+            // 
+            // rChono
+            // 
+            this.rChono.Location = new System.Drawing.Point(155, 345);
+            this.rChono.Name = "rChono";
+            this.rChono.Size = new System.Drawing.Size(87, 30);
+            this.rChono.TabIndex = 4;
+            this.rChono.Text = "Reset";
+            this.rChono.UseVisualStyleBackColor = true;
+            this.rChono.Click += new System.EventHandler(this.rChono_Click);
+            // 
+            // labelAction
+            // 
+            this.labelAction.AutoSize = true;
+            this.labelAction.Location = new System.Drawing.Point(56, 39);
+            this.labelAction.Name = "labelAction";
+            this.labelAction.Size = new System.Drawing.Size(55, 17);
+            this.labelAction.TabIndex = 5;
+            this.labelAction.Text = "Action :";
+            // 
+            // comboBoxActions
+            // 
+            this.comboBoxActions.FormattingEnabled = true;
+            this.comboBoxActions.Location = new System.Drawing.Point(137, 39);
+            this.comboBoxActions.Name = "comboBoxActions";
+            this.comboBoxActions.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxActions.TabIndex = 6;
+            // 
+            // labelAddAction
+            // 
+            this.labelAddAction.AutoSize = true;
+            this.labelAddAction.Location = new System.Drawing.Point(322, 39);
+            this.labelAddAction.Name = "labelAddAction";
+            this.labelAddAction.Size = new System.Drawing.Size(131, 17);
+            this.labelAddAction.TabIndex = 7;
+            this.labelAddAction.Text = "Ajouter une action :";
+            // 
+            // textBoxAddAction
+            // 
+            this.textBoxAddAction.Location = new System.Drawing.Point(460, 39);
+            this.textBoxAddAction.Name = "textBoxAddAction";
+            this.textBoxAddAction.Size = new System.Drawing.Size(131, 22);
+            this.textBoxAddAction.TabIndex = 8;
+            // 
+            // buttonAddAction
+            // 
+            this.buttonAddAction.Location = new System.Drawing.Point(598, 37);
+            this.buttonAddAction.Name = "buttonAddAction";
+            this.buttonAddAction.Size = new System.Drawing.Size(75, 26);
+            this.buttonAddAction.TabIndex = 9;
+            this.buttonAddAction.Text = "Ajouter";
+            this.buttonAddAction.UseVisualStyleBackColor = true;
+            this.buttonAddAction.Click += new System.EventHandler(this.buttonAddAction_Click);
+            // 
+            // SaveAction
+            // 
+            this.SaveAction.Location = new System.Drawing.Point(419, 232);
+            this.SaveAction.Name = "SaveAction";
+            this.SaveAction.Size = new System.Drawing.Size(110, 59);
+            this.SaveAction.TabIndex = 10;
+            this.SaveAction.Text = "Creer Sauvegarde";
+            this.SaveAction.UseVisualStyleBackColor = true;
+            this.SaveAction.Click += new System.EventHandler(this.SaveAction_Click);
+            // 
+            // saveActionTextbox
+            // 
+            this.saveActionTextbox.Location = new System.Drawing.Point(419, 138);
+            this.saveActionTextbox.Name = "saveActionTextbox";
+            this.saveActionTextbox.Size = new System.Drawing.Size(254, 22);
+            this.saveActionTextbox.TabIndex = 11;
+            // 
+            // updateAction
+            // 
+            this.updateAction.Location = new System.Drawing.Point(550, 232);
+            this.updateAction.Name = "updateAction";
+            this.updateAction.Size = new System.Drawing.Size(123, 59);
+            this.updateAction.TabIndex = 12;
+            this.updateAction.Text = "Mettre à jour";
+            this.updateAction.UseVisualStyleBackColor = true;
+            this.updateAction.Click += new System.EventHandler(this.updateAction_Click);
+            // 
+            // labelListAction
+            // 
+            this.labelListAction.AutoSize = true;
+            this.labelListAction.Location = new System.Drawing.Point(419, 345);
+            this.labelListAction.Name = "labelListAction";
+            this.labelListAction.Size = new System.Drawing.Size(0, 17);
+            this.labelListAction.TabIndex = 13;
+            // 
+            // textBoxListAction
+            // 
+            this.textBoxListAction.AcceptsReturn = true;
+            this.textBoxListAction.Location = new System.Drawing.Point(419, 345);
+            this.textBoxListAction.Multiline = true;
+            this.textBoxListAction.Name = "textBoxListAction";
+            this.textBoxListAction.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxListAction.Size = new System.Drawing.Size(251, 188);
+            this.textBoxListAction.TabIndex = 14;
+            this.textBoxListAction.WordWrap = false;
+            // 
+            // importSaveAction
+            // 
+            this.importSaveAction.Location = new System.Drawing.Point(419, 166);
+            this.importSaveAction.Name = "importSaveAction";
+            this.importSaveAction.Size = new System.Drawing.Size(110, 48);
+            this.importSaveAction.TabIndex = 15;
+            this.importSaveAction.Text = "Importer actions client";
+            this.importSaveAction.UseVisualStyleBackColor = true;
+            this.importSaveAction.Click += new System.EventHandler(this.importSaveAction_Click);
+            // 
+            // openFileDialogImportAction
+            // 
+            this.openFileDialogImportAction.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1119, 783);
+            this.Controls.Add(this.importSaveAction);
+            this.Controls.Add(this.textBoxListAction);
+            this.Controls.Add(this.labelListAction);
+            this.Controls.Add(this.updateAction);
+            this.Controls.Add(this.saveActionTextbox);
+            this.Controls.Add(this.SaveAction);
+            this.Controls.Add(this.buttonAddAction);
+            this.Controls.Add(this.textBoxAddAction);
+            this.Controls.Add(this.labelAddAction);
+            this.Controls.Add(this.comboBoxActions);
+            this.Controls.Add(this.labelAction);
+            this.Controls.Add(this.rChono);
+            this.Controls.Add(this.labelChrono);
+            this.Controls.Add(this.sChrono);
+            this.Controls.Add(this.cStart);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cStart;
+        private System.Windows.Forms.Button sChrono;
+        private System.Windows.Forms.Label labelChrono;
+        private System.Windows.Forms.Timer chronoTimer;
+        private System.Windows.Forms.Button rChono;
+        private System.Windows.Forms.Label labelAction;
+        private System.Windows.Forms.ComboBox comboBoxActions;
+        private System.Windows.Forms.Label labelAddAction;
+        private System.Windows.Forms.TextBox textBoxAddAction;
+        private System.Windows.Forms.Button buttonAddAction;
+        private System.Windows.Forms.Button SaveAction;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogAction;
+        private System.Windows.Forms.TextBox saveActionTextbox;
+        private System.Windows.Forms.Button updateAction;
+        private System.Windows.Forms.Label labelListAction;
+        private System.Windows.Forms.TextBox textBoxListAction;
+        private System.Windows.Forms.Button importSaveAction;
+        private System.Windows.Forms.OpenFileDialog openFileDialogImportAction;
+    }
+}
+
---
+++ ChronoAspera/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChronoAspera
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxActions.Items.AddRange(new object[] { "Appel","Redaction consultation", "Recherche juridique","Réponse au mail"});
+            saveFileDialogAction.Filter = "Fichiers texte|*.txt|Tous les fichiers|*.*";
+            saveFileDialogAction.FilterIndex = 2;
+            saveFileDialogAction.RestoreDirectory = true;
+
+
+        }
+
+        private void cStart_Click(object sender, EventArgs e)
+        {
+            {
+                
+                stopwatch.Start();
+                chronoTimer.Start();
+                chronoTimer.Tick += ChronoTimer_Tick;
+
+            }
+        }
+
+        private void ChronoTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            labelChrono.Text = elapsedTime;
+        }
+
+        private void sChrono_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+            
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            labelChrono.Text = elapsedTime;
+            chronoTimer.Stop();
+
+        }
+
+        private void rChono_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+           ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            labelChrono.Text = elapsedTime;
+
+            }
+
+        private void buttonAddAction_Click(object sender, EventArgs e)
+        {
+            comboBoxActions.Items.Add(textBoxAddAction.Text);
+            textBoxAddAction.Text = string.Empty; 
+        }
+
+        private void SaveAction_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialogAction.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    Stream myStream = saveFileDialogAction.OpenFile();
+
+                    if (myStream != null)
+                    {
+                        using (myStream)
+                        {
+                            using (StreamWriter writer = new StreamWriter(myStream, Encoding.UTF8))
+                            {
+                                writer.WriteLine(labelChrono.Text + " - " + comboBoxActions.SelectedItem);
+                                saveActionTextbox.Text = saveFileDialogAction.FileName;
+                                labelListAction.Text = "Liste des actions : ";
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur survenue lors de la sauvegarde : " + ex.Message);
+                }
+
+                using (StreamReader rd = new StreamReader(saveActionTextbox.Text))
+                {
+                    string line;
+                    while ((line = rd.ReadLine()) != null)
+                    {
+                        textBoxListAction.AppendText(line);
+                    }
+                }
+            }
+        }
+
+        private void updateAction_Click(object sender, EventArgs e)
+        {
+            textBoxListAction.Clear();
+            using (StreamWriter wr = File.AppendText(saveActionTextbox.Text))
+            {
+                wr.WriteLine(labelChrono.Text + " - " + comboBoxActions.SelectedItem);
+
+            }
+            using (StreamReader rd = new StreamReader(saveActionTextbox.Text))
+            {
+                string line;
+                while ((line = rd.ReadLine()) != null)
+                {
+                    textBoxListAction.AppendText(line + Environment.NewLine);
+
+                }
+
+            }
+        }
+
+        private void importSaveAction_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogImportAction.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Stream mySteam = openFileDialogImportAction.OpenFile();
+
+                    if (mySteam != null)
+                    {
+                        using (mySteam)
+                        {
+                            saveActionTextbox.Text = openFileDialogImportAction.FileName;
+                        }
+                        using (StreamReader rd = new StreamReader(saveActionTextbox.Text))
+                        {
+                            string line;
+                            while ((line = rd.ReadLine()) != null)
+                            {
+                                textBoxListAction.AppendText(line + Environment.NewLine);
+
+                            }
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur survenue lors de la récupération : " + ex.Message);
+                }
+            }
+        }
+    }
+}
